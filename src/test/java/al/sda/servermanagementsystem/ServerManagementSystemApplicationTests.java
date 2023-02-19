7424c3387e5c4a98cdf80405d454ef91d6ab66a3
ErtiDesu <erti.rakipaj@gmail.com>
2023-02-19 14:42:47 +0100

    Added new project files

---
+++ src/test/java/al/sda/servermanagementsystem/ServerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package al.sda.servermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
