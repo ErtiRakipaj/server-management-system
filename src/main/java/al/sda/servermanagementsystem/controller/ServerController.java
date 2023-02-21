4b5a1162812fe4e27ba327f37cdf66dca6be66e0
ElvisRexhepi <elvisrexhepi55@gmail.com>
2023-02-21 12:12:34 +0100

    Added crud operations on service

--- src/main/java/al/sda/servermanagementsystem/controller/ServerController.java
+++ src/main/java/al/sda/servermanagementsystem/controller/ServerController.java
@@ -1,4 +1,12 @@
 package al.sda.servermanagementsystem.controller;
 
+import al.sda.servermanagementsystem.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ServerController {
+
+    private final ServerService serverService;
 }
