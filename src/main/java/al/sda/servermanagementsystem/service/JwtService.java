752c72f8f56b7590b94ad224269fda55fcf0ea77
ErtiDesu <erti.rakipaj@gmail.com>
2023-02-26 16:48:43 +0100

    Fixed logout feature via token blacklisting

--- src/main/java/al/sda/servermanagementsystem/service/JwtService.java
+++ src/main/java/al/sda/servermanagementsystem/service/JwtService.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,7 +38,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 300000))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
@@ -46,8 +48,8 @@ public class JwtService {
         return(username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
-    private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(Date.from(Instant.now()));
     }
 
     private Date extractExpiration(String token) {
@@ -72,6 +74,19 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
 
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+    public void invalidateToken(String token) {
+
+        Claims claims = extractAllClaim(token);
+
+        claims.setExpiration(Date.from(Instant.now()));
+
+         Jwts
+                .builder()
+                .setClaims(claims)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+
     }
 
 }
